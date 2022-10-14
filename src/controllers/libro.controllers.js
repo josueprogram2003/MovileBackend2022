32d0da23cf5b2afa526bee11705941a40cb7b622
josueprogram2003 <josuedominguezonline12@gmail.com>
2022-10-13 23:14:22 -0500

    jd

--- src/controllers/libro.controllers.js
+++ src/controllers/libro.controllers.js
@@ -1,37 +1,117 @@
-import { pool } from "../database";
-export const getBibliotecas = async (req, res) => {
+import { pool } from "../database"
+
+
+
+export const readAllLibro=async(req,res)=>{
+  
   try {
     pool.query(
-      "select l.idlibro, l.titulo, l.autor, l.paginas, e.nombre from libro l join editorial e on e.ideditorial = l.ideditorial;",
+      "select l.titulo, l.autor, l.paginas, e.nombre from libro l join editorial e on e.ideditorial = l.ideditorial;",
+
       function (err, result) {
         console.log(result);
         try {
           return res.status(200).json(result);
         } catch (error) {
-          return res.status(500).json("Error al listar libro");
+          return res.status(500).json("Error al listar escuela");
         }
       }
     );
   } catch (error) {
-    return res.status(500).json("Error al listar libro");
+    return res.status(500).json("Error al listar escuela");
   }
 };
-export const getBibliotecasId = async (req, res) => {
+
+export const agregarLibro = async (req,res)=>{
+
+  try {
+    const titulo = req.body.titulo;
+    const autor = req.body.autor;
+    const paginas = parseInt(req.body.paginas);
+    const editor= req.body.editor;
+    const ideditorial = parseInt(req.body.ideditorial);
+    pool.query(
+      "INSERT INTO libro (titulo, autor, paginas, editor, ideditorial) VALUES(?,?,?,?,?); ",
+      [titulo, autor, paginas, editor, ideditorial],
+      function (err, result) {
+        console.log(result);
+        try {
+          return res.status(200).json(result);
+        } catch (error) {
+          return res.status(500).json("Error al listar los libros");
+          
+        }
+
+      }
+    );
+  } catch (error) {
+    return res.status(500).json("Error al listar escuela");
+  }
+};
+
+  
+
+
+export const getLibroId = async (req,res)=>{
+  const id = parseInt(req.params.id);
+  pool.query('select * from libro where idlibro = ?;',[id], function(err, result) {
+  try {
+      return res.status(200).json(result);
+  } catch (e) {
+      return res.status(500).json('Error al mostrar el producto');
+  }
+});
+};
+
+
+
+
+
+export const deleteLibro = async (req,res)=>{
   try {
     const id = parseInt(req.params.id);
+
     pool.query(
-      "select * from libro where idlibro = ?;",
+      "DELETE FROM libro WHERE idlibro=?; ",
       [id],
       function (err, result) {
-        console.log(result);
         try {
           return res.status(200).json(result);
         } catch (error) {
-          return res.status(500).json("Error al listar bibloteca");
+          return res.status(500).json("Error al listar escuela");
         }
       }
     );
   } catch (error) {
-    return res.status(500).json("Error al listar bibloteca");
+    return res.status(500).json("Error al listar escuela");
   }
 };
+
+
+
+
+export const editarLibro = async (req,res)=>{
+  try {
+    const id = parseInt(req.params.id);
+    const titulo = req.body.titulo;
+    const autor = req.body.autor;
+    const paginas = parseInt(req.body.paginas);
+    const editor= req.body.editor;
+    const ideditorial = parseInt(req.body.ideditorial);
+    
+    pool.query(
+      "update libro SET titulo=?, autor=? , paginas=? , editor=? , ideditorial=? WHERE idlibro=?; ",
+      [titulo, autor, paginas, editor, ideditorial, id],
+      function (err, result) {
+        try {
+          return res.status(200).json(result);
+        } catch (error) {
+          return res.status(500).json("Error al listar escuela");
+        }
+      }
+    );
+  } catch (error) {
+    return res.status(500).json("Error al listar escuela");
+  }
+};
+
--- src/routes/libro.routes.js
+++ src/routes/libro.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import * as bibliotecas from "../controllers/libro.controllers";
 const router = Router();
-router.get("/listar", bibliotecas.getBibliotecas);
-router.get("/listar/:id", bibliotecas.getBibliotecasId);
+router.get("/listar", bibliotecas.readAllLibro);
+router.get("/listar/:id", bibliotecas.getLibroId);
+router.delete("/delete", bibliotecas.deleteLibro);
+router.post("/create", bibliotecas.agregarLibro);
+router.put("/update", bibliotecas.editarLibro);
+
 export default router;
